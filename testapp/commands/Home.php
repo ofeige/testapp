4a812a00e6996613631a76ee3f0fa145111de8d8
Oliver Feige <feige@olivers-mbp.fritz.box>
2014-02-01 12:53:54 +0100

    Switch from PHP to Twig Template Engine
    Implement all features with twig
    Implement Signup System with Forms

--- testapp/commands/Home.php
+++ testapp/commands/Home.php
@@ -3,8 +3,14 @@
 namespace Opf\Mvc;
 
 use Opf\Auth\AuthEventHandler;
+use Opf\Form\Elements\Button;
+use Opf\Form\Elements\Input;
+use Opf\Form\Elements\Password;
+use Opf\Form\Form;
+use Opf\Form\Rules\Min;
 use Opf\Registry\Registry;
 use Opf\Template\ViewTwig;
+
 
 class Home extends CommandAbstract
 {
@@ -19,12 +25,42 @@ class Home extends CommandAbstract
         $view->render($this->request, $this->response);
     }
 
-    public function info()
+    public function signup()
     {
-        $view = new ViewTwig('info');
-        $view->assign('session', Registry::getInstance()->getSession());
+        $form = new Form();
 
-        $view->assign('iniget', ini_get_all());
+        $input = new Input('user', 'Benutzer', 'Benutzername hier eingeben');
+        $input->setRequired('Benutzername nicht vorhanden');
+
+        $password1 = new Password('password1', 'Passwort', 'Passwort hier eingeben');
+        $password1->setRequired('Passwort nicht vorhanden')->addRule(new Min('Passwort ist zu kurz', 5));
+
+        $password2 = new Password('password2', 'Passwort Wiederholung', 'Passwort hier eingeben');
+        $password2->setRequired('Passwort nicht vorhanden');
+
+        $button = new Button('Sign Up');
+
+        $form->addElement($input);
+        $form->addElement($password1);
+        $form->addElement($password2);
+        $form->addElement($button);
+
+        if($form->isValid($this->request)) {
+            /** put code for saving data here */
+            $html = '<p>save data</p>';
+        }
+        else {
+            $html = (string) $form;
+        }
+
+
+
+
+        $view = new ViewTwig('signup');
+        $view->assign('session', Registry::getInstance()->getSession());
+        $view->assign('form', $html);
+        $view->assign('authUsername', AuthEventHandler::authName);
+        $view->assign('authPassword', AuthEventHandler::authPassword);
         $view->render($this->request, $this->response);
     }
 }
