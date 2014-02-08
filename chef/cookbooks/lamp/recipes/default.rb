a931bdc84ced510234f872083ac4f4cc4a5fd814
Oliver Feige <ofeige@gmx.de>
2014-02-08 11:34:22 +0100

    create dedicated mysql user for the testapp

--- chef/cookbooks/lamp/recipes/default.rb
+++ chef/cookbooks/lamp/recipes/default.rb
@@ -12,24 +12,38 @@ include_recipe "apache2::mod_headers"
 include_recipe "database::mysql"
 
 
+mysql_connection_info = {
+  :host     => 'localhost',
+  :username => 'root',
+  :password => node['mysql']['server_root_password']
+}
+
 mysql_database 'myapp' do
-  connection(
-    :host     => 'localhost',
-    :username => 'root',
-    :password => node['mysql']['server_root_password']
-  )
+  connection mysql_connection_info
   action :create
 end
 
 #or import from a dump file
 mysql_database "myapp" do
-  connection(
-      :host     => 'localhost',
-      :username => 'root',
-      :password => node['mysql']['server_root_password']
-    )
+  connection mysql_connection_info
   sql { ::File.open('/vagrant/data/mysql.schema.sql').read }
   action :query
+end
+
+database_user 'myapp_user' do
+  connection mysql_connection_info
+  password   'super_secret'
+  provider   Chef::Provider::Database::MysqlUser
+  action     :create
+end
+
+mysql_database_user 'myapp_user' do
+  connection    mysql_connection_info
+  password      'super_secret'
+  database_name 'myapp'
+  host          '%'
+  privileges    [:all]
+  action        :grant
 end
 
 package "php5-mysqlnd" do
