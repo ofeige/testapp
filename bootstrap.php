6385b38e635d993a7fa98e74234c8e4e2281c311
Oliver Feige <feige@olivers-mbp.fritz.box>
2014-02-09 21:57:49 +0100

    Use now the MySQL Auth Driver
    and a lot of cleanups

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,7 @@ $OPF_START = microtime(true);
 $loader = require ('vendor/autoload.php');
 $loader->add('', __DIR__.'/testapp/models');
 
+use Opf\Auth\Driver\Mysql;
 use Opf\Registry\Registry;
 use Opf\Mvc\CommandResolver;
 use Opf\Mvc\Controller;
@@ -25,18 +26,11 @@ $session = new Php();
 
 Registry::getInstance()->setSession($session);
 
-$driver = new \Opf\Auth\Driver\PhpArray(array(
-   'ofeige' => 'test',
-   'test'   => 'test'
-));
-
-$driver = new \Opf\Auth\Driver\Mysql('User');
-
-//$auth = new \Opf\Auth\Auth($driver, ,$session);
-
+$driver = new Mysql('User');
 $login = new \Opf\Template\ViewTwig('signin');
 $auth = new \Opf\Auth\AuthEventHandler($driver, $session, $request, $response, $login);
 
+try {
 \Opf\Event\Dispatcher::getInstance()->addHandler('CommandConstructor', $auth);
 
 
@@ -44,4 +38,10 @@ $resolver = new CommandResolver(OPF_APPLICATION_PATH, 'Home');
 $controller = new Controller($resolver);
 $controller->handleRequest($request, $response);
 
+}
+catch (\Exception $e) {
+    echo '<pre>';
+    print_r($e);
+    echo '</pre>';
+}
 printf("\n<!-- page generation time: %s -->", (microtime(true) - $OPF_START));
