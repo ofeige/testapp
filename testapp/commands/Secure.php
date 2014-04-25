23489111f72285d5e9d2b217496e579c5af5cc99
Oliver Feige <oliver@feige.net>
2014-04-25 22:15:27 +0200

    show image

--- testapp/commands/Secure.php
+++ testapp/commands/Secure.php
@@ -15,4 +15,21 @@ class Secure extends CommandAbstract
         $view->assign('session', Registry::getInstance()->getSession());
         $view->render($this->request, $this->response);
     }
+
+    public function img()
+    {
+        $picture = \Pictures::find_one($this->request->getParameter('id'));
+
+       if($picture == false) {
+            $this->response->addHeader('Content-type', 'image/svg+xml');
+            $this->response->addHeader('Content-length', filesize(OPF_APPLICATION_PATH . '/../public/test_screen.svg'));
+            $this->response->write(file_get_contents(OPF_APPLICATION_PATH . '/../public/test_screen.svg'));
+        }
+        else {
+            $info = getimagesizefromstring($picture->data);
+            $this->response->addHeader('Content-type', image_type_to_mime_type($info[2]));
+            $this->response->addHeader('Content-length', strlen($picture->data));
+            $this->response->write($picture->data);
+        }
+    }
 }
